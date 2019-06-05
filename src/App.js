a28c3c5a10eb095de8d187a9169eee04f625a75c
Ahuoiza <oizabaiye@gmail.com>
2019-06-05 19:55:55 -0400

    created basic speaker form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import theme from './theme'
 import Header from './Header'
 import Nav from './Nav'
 import Entity from './Entity'
+import SpeakerForm from './SpeakerForm';
 
 export default () => (
   <BrowserRouter>
@@ -16,6 +17,7 @@ export default () => (
         <Box p="24px">
           <Route path="/:entity" component={Entity} />
         </Box>
+        <SpeakerForm />
       </>
     </ThemeProvider>
   </BrowserRouter>
