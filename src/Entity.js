089a48d4e8ace0729b46dd696719b7a95c494235
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-10-03 01:28:29 -0400

    added basic layout for speaker form using controlled inputs

--- src/Entity.js
+++ src/Entity.js
@@ -7,7 +7,7 @@ import { BASE_URL, WORKSPACE, API_KEY } from './constants'
 import { parseJSON } from './utils'
 import * as entities from './entities'
 import SpeakerBar from './components/SpeakerBar';
-import SortByTopic from './services/SortByTopic'
+import sortByTopic from './services/sortByTopic'
 
 let Entity = ({ match, location }) => {
   let [records, setRecords] = useState([])
@@ -35,7 +35,7 @@ let Entity = ({ match, location }) => {
   )
 
   const searchTopic = (topic) => {
-    const arr = SortByTopic(topic, records);
+    const arr = sortByTopic(topic, records);
     setRecords(arr);
   }
 
