3c40a6d40a5c4ee30116d9ea016194a7e6654d86
Thiago <thiagopereira2634@gmail.com>
2019-10-04 09:03:17 -0400

    Update header according to mock-up

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { Box } from 'rebass'
 import theme from './theme'
 import Header from './Header'
-import Nav from './Nav'
+
 import Entity from './Entity'
-import SpeakerForm from './SpeakerForm';
+import SpeakerForm from './SpeakerForm'
 
 export default () => (
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <>
         <Header />
-        <Nav />
         <Box p="24px">
           <Route path="/:entity" component={Entity} />
           <Route path="/speaker/submit" component={SpeakerForm} />
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
 import { Flex, Box, Text } from 'rebass'
+import styled from 'styled-components'
+import Nav from './Nav'
+
+let Logo = styled(Text)`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${props => props.theme.colors.primary};
+  letter-spacing: 1.2px;
+  height: 100%;
+  line-height: 44px;
+  @media (min-width: 768px) {
+    font-size: 24px;
+  }
+`
 
 export default () => (
-  <Flex justifyContent="center">
-    <Box p="64px">
+  <Flex justifyContent="spaceBetween">
+    <Box p="8px">
       <Fade bottom>
-        <Text
-          fontSize={['16px', '32px', '64px']}
-          fontWeight={700}
-          color="#000"
-          letterSpacing={1.2}
-        >
-          speakfriend
-        </Text>
+        <Logo>speakfriend</Logo>
       </Fade>
+    </Box>
+    <Box p="8px" width={1}>
+      <Nav />
     </Box>
   </Flex>
 )
