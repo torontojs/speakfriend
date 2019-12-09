6cc2b2c72104a2f6c52b0827b6731eaf73ad6970
SonuToor <savrooptoor92@gmail.com>
2019-12-09 18:04:45 -0500

    displayed all the event info

--- src/entities/Event.js
+++ src/entities/Event.js
@@ -13,29 +13,44 @@ export default ({ fields }) => {
   const event = fields['Event']
   const description = fields['Description']
   const email = fields['Email']
-  // const date = fields['Date']
-  // const location = fields['Location']
+  const organization = fields['Organization']
+  const date = fields['Date']
+  const location = fields['Location']
   return (
     <FlexCard alignItems="center" width={'100%'}>
       <Box px={[2, 3]} mt={[3, 0]} alignSelf="stretch" flex="1">
         <Text fontSize={4} fontWeight="500">
-          {`Events: ${event}`}
+          {`Event: ${event}`}
         </Text>
         <Text fontSize={2} fontWeight="200" marginTop="3%">
           {description}
         </Text>
+        <Flex
+          py={2}
+          mt={2}
+          alignItems="center"
+          marginTop="3%"
+          justifyContent="space-around"
+          marginBottom="5%"
+        >
+          <Text>{`Presented By: ${organization}`}</Text>
+          <Text>{date}</Text>
+          <Text>{location}</Text>
+        </Flex>
+        <Flex display="flex" justifyContent="flex-end">
+          <StyledButton
+            text={
+              <a
+                href={`mailto:${email}`}
+                target="_blank"
+                style={{ color: 'inherit', textDecoration: 'none' }}
+              >
+                contact organizer
+              </a>
+            }
+          />
+        </Flex>
       </Box>
-      <StyledButton
-        text={
-          <a
-            href={`mailto:${email}`}
-            target="_blank"
-            style={{ color: 'inherit', textDecoration: 'none' }}
-          >
-            contact organizer
-          </a>
-        }
-      />
     </FlexCard>
   )
 }
